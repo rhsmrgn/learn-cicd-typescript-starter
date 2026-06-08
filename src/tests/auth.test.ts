d8aa7e3a7bbd4aa717685c89bc7d7ff6eb6260c6
rhsmrgn <rhsmrgn@gmail.com>
2026-06-08 09:30:04 -0700

    added style check

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,20 +2,22 @@ import { describe, expect, test } from "vitest";
 import { getAPIKey } from "../api/auth.js";
 import { IncomingHttpHeaders } from "http";
 
-const fakeHeader: IncomingHttpHeaders = {"authorization": undefined};
-const stillNullHeader: IncomingHttpHeaders = {"authorization": "too short"};
-const realHeader: IncomingHttpHeaders = {"authorization": "hey i think i'm long enough"};
+const fakeHeader: IncomingHttpHeaders = { authorization: undefined };
+const stillNullHeader: IncomingHttpHeaders = { authorization: "too short" };
+const realHeader: IncomingHttpHeaders = {
+  authorization: "hey i think i'm long enough",
+};
 
 describe("getAPIKey", () => {
-    test("check if function returns null on undefined", () => {
-        expect(getAPIKey(fakeHeader)).toBeNull();
-    });
+  test("check if function returns null on undefined", () => {
+    expect(getAPIKey(fakeHeader)).toBeNull();
+  });
 
-    test("check API length", () => {
-        expect(getAPIKey(stillNullHeader)).toBeNull();
-    });
+  test("check API length", () => {
+    expect(getAPIKey(stillNullHeader)).toBeNull();
+  });
 
-    test("api is long enough", () => {
-        expect(getAPIKey(realHeader)).toBeNull();
-    });
+  test("api is long enough", () => {
+    expect(getAPIKey(realHeader)).toBeNull();
+  });
 });
